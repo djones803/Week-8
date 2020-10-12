06ab8913bb176ba55af77e15397c6cae658f6f5f
djones803 <wdj@usca.edu>
2020-10-12 13:08:57 -0400

    1 file modified, 1 added

---
+++ arith.js
@@ -0,0 +1,11 @@
+//get the element
+
+// element --- property
+var num1 = parseInt(document.getElementById('n1').textContent);
+var num2 = parseInte(document.getElementById('n2').textContent);
+
+//processs
+var c = num1 + num2;
+
+//display
+document.write(c);
